7f9764360c634cf614c2400796d8fb828561f039
unknown <mikael_tofvesson@hotmail.com>
2019-02-18 22:40:31 +0100

    app

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -11,14 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <a href="http://localhost:5000/auth/facebook">Sign in with facebook</a>
         </header>
       </div>
     );
